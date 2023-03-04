5864dea65fad787d73b24b9815fd78fe81a9f0fc
Ario-sut <rakaario6@gmail.com>
2023-03-05 06:46:59 +0700

    Background Changer Project

---
+++ transparent.py
@@ -0,0 +1,17 @@
+import requests
+from datetime import datetime
+from key import API_TOKEN_KEY
+
+def BgChanger(img):
+    response = requests.post(
+        'https://api.remove.bg/v1.0/removebg', files={'image_file':open(img, 'rb')}, 
+        data={'size':'auto'}, 
+        headers={'X-Api-Key': API_TOKEN_KEY},)
+    
+    if response.status_code == requests.codes.ok:
+        with open('Output-Images/hasilnya-%s.png' %datetime.now().strftime('%Y-%m-%d_%H-%M-%S'), 'wb') as out:
+            out.write(response.content)
+    else:
+        print('Error :', response.status_code, response.text)
+
+BgChanger(img = 'Background-images/389564.jpg')
